57653fb0259c83bb79fffc9daa675714328d6895
Gabriel Moreti Oliveira <69165202+gmti87@users.noreply.github.com>
2026-06-09 20:56:28 -0300

    Refactor CORS settings and port configuration
    Updated CORS configuration to allow specific origins and made port configurable via environment variable. Atualizando o projeto para rodar o frontend pelo github pages e o backend local junto com o banco.

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,12 +8,32 @@ const session = require("express-session");
 
 const app = express();
 
-const port = 3000;
+//const port = 3000;
+const port = process.env.PORT || 3000;
 
 //app.use(cors());
 
+/*
 app.use(cors({
     origin: "http://localhost:5500",
+    credentials: true
+}));
+*/
+
+const allowedOrigins = [
+    "http://localhost:5500",
+    "https://gmti87.github.io",
+    "https://gmti87.github.io/Fatec-PI-2Sem"
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Bloqueado pelo CORS"));
+        }
+    },
     credentials: true
 }));
 
